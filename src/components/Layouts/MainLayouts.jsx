5222969c750281e1aebbf8024bc64cad7c25f35c
gaston-magrassi <gas051093@hotmail.com.ar>
2025-08-27 14:11:25 -0300

    modificacion del head

--- src/components/Layouts/MainLayouts.jsx
+++ src/components/Layouts/MainLayouts.jsx
@@ -1,12 +1,18 @@
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
-function MainLayouts (){
-    return(
-        <div className="d-flex">
-            <Sidebar />
-                <Outlet />
-        </div>
-    )
+import Header from "./Header";
+import { useLocation } from "react-router-dom";
+function MainLayouts() {
+    const location = useLocation()
+  return (
+    <div className="d-flex">
+      <Sidebar />
+      <div>
+        <Header title={location.pathname} subTitle={ location.pathname} />
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default MainLayouts;
